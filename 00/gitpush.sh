c0e22726f9d7e881b3d00950e11b07a4a39bde94
oo <ovwxxwvo@foxmail.com>
2026-07-08 09:55:31 +0800

    feat: subject...

--- 00/gitpush.sh
+++ 00/gitpush.sh
@@ -6,7 +6,8 @@ cd "${script_dir}" && cd ../ && pwd
 function git_push() {
     echo "---------- ---------- ---------- ----------"
     # git remote add upstream git@github.com:gevico/gevico-classroom-qemu-camp-2026-exper-qemu-camp-2026-exper.git
-    # git pull upstream main --rebase
+    git pull upstream main --rebase
+    # git pull origin main --rebase
     echo "---------- ---------- ---------- ----------"
     git add .
     echo "---------- ---------- ---------- ----------"
