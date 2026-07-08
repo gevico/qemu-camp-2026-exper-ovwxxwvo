ec7a98281d883cca60d184845fc64434714a2384
oo <ovwxxwvo@foxmail.com>
2026-07-08 10:50:38 +0800

    feat: subject...

--- 00/gitfix.sh
+++ 00/gitfix.sh
@@ -3,5 +3,29 @@
 script_dir=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 cd "${script_dir}" && cd ../ && pwd
 
-echo "---------- ---------- ---------- ----------"
+branch="fix/rust-workspace-member-add-i2c"
+commit="fix(rust): add hw/i2c to workspace members"
 
+function git_fix() {
+
+    echo "---------- ---------- ---------- ----------"
+    # git stash
+    git fetch origin
+    git reset --hard origin/main
+    echo "---------- ---------- ---------- ----------"
+    git checkout -b "$branch"
+
+    echo "---------- ---------- ---------- ----------"
+    read -p "Press Enter to continue after fix."
+
+    echo "---------- ---------- ---------- ----------"
+    git add rust/Cargo.toml
+    git commit -m "$commit"
+    git push origin "$branch"
+
+    echo "---------- ---------- ---------- ----------"
+    git checkout main
+
+    }
+
+git_fix
